6a6348157837169b8dced1bf4738948b005a7252
Austin He <ahe8@my.bcit.ca>
2024-06-25 16:39:15 -0700

    add exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,21 +2,29 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' , number: '040-123456'}
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filterCriteria, setFilterCriteria] = useState('')
 
   const handleChange = (event, setFunction) => {
     setFunction(event.target.value)
   }
 
-  const personsArray = persons.map((person) => <p key={person.name}>{person.name} {person.number}</p>)
+  const personsArray = 
+  filterCriteria === '' ?
+  persons.map((person) => <p key={person.name}>{person.name} {person.number}</p>)
+  :
+  persons.filter((person) => person.name.toLowerCase().startsWith(filterCriteria)).map((person) => <p key={person.name}>{person.name} {person.number}</p>)
 
   const addPerson = (event) => {
     event.preventDefault();
 
-    const results = persons.filter(person => person.name === newName);
+    const results = persons.filter(person => person.name.toLowerCase() === newName);
 
     results.length !== 0 ?
       alert(`${newName} is already added to phonebook`)
@@ -28,6 +36,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={filterCriteria} onChange={(e) => handleChange(e, setFilterCriteria)}/></div>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={(e) => handleChange(e, setNewName)}/>
