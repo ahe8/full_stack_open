9f8971604980424171129078e935631dc78bd69c
Austin He <ahe8@my.bcit.ca>
2024-06-25 11:19:30 -0700

    1.11 change stats into table

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,5 +1,20 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+  return (
+    <button onClick={() => props.click(props.value + 1)}>{props.text}</button>
+  )
+}
+
+const StatisticLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
+}
+
 const Statistics =  (props) => {
   const { good, neutral, bad } = props;
 
@@ -9,14 +24,27 @@ const Statistics =  (props) => {
     total > 0 ?
       <>
         <h1>statistics</h1>
+      
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good}/>
+            <StatisticLine text="neutral" value={neutral}/>
+            <StatisticLine text="bad" value={bad}/>
 
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-
-        <p>all {total}</p>
-        <p>average {(good - bad) / (total) || 0}</p>
-        <p>positive {(good / (total)) * 100 || 0}%</p>
+            <tr>
+              <td>all</td>
+              <td>{total}</td>
+            </tr>
+            <tr>
+              <td>average</td>
+              <td>{(good - bad) / (total) || 0}</td>
+            </tr>
+            <tr>
+              <td>positive</td>
+              <td>{(good / (total)) * 100 || 0}%</td>
+            </tr>
+          </tbody>
+        </table>
       </>
     :
     <p>No feedback given</p>
@@ -32,9 +60,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button text="good" value={good} click={setGood}/>
+      <Button text="neutral" value={neutral} click={setNeutral}/>
+      <Button text="bad" value={bad} click={setBad}/>
 
       <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
