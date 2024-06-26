ef8c1419be97eeaebb14bd9df2f0263bcf18f6f2
Austin He <ahe8@my.bcit.ca>
2024-06-26 15:52:30 -0700

    add up to 2.19

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import countryService from './services/countries'
+import Country from "./components/Country";
+
+const App = () => {
+  const [countryName, setCountryName] = useState('')
+  const [countries, setCountries] = useState([])
+  const [showCountry, setShowCountry] = useState({})
+
+  useEffect(() => {
+    countryService.getAll()
+    .then(data => { 
+      setCountries(data);
+      
+      let showCountryInit = {};
+      data.forEach(country => {
+        showCountryInit[country['name']['common']] = false;
+      })
+      setShowCountry(showCountryInit);
+    })
+    .catch(err => console.log(err))
+  }, [])
+
+
+  const handleChange = (e) => {
+    setCountryName(e.target.value);
+  }
+
+  const filteredCountries = countries
+    .filter((country) => {
+      return country['name']['common'].toLowerCase().startsWith(countryName.toLowerCase());
+    })
+    
+
+  const toggleCountry = (country) => {
+    const currCountryName = country['name']['common'];
+
+    let newCountryObj = {...showCountry}
+    newCountryObj[currCountryName] = !showCountry[currCountryName];
+
+    setShowCountry(newCountryObj);
+  }
+
+  const countriesArray = filteredCountries.map((country) => {
+    const currCountryName = country['name']['common'];
+
+    return (
+      <div key={country['cca2']}>
+        {currCountryName} <button onClick={() => toggleCountry(country)}>{showCountry[currCountryName] ? "hide" : "show"}</button>
+        {showCountry[currCountryName] && <Country country={country}/>}
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      find countries<input value={countryName} onChange={handleChange}/>
+      {countryName && (
+        filteredCountries.length === 1 ? <Country country={filteredCountries[0]}/> : countriesArray
+      )}
+    </div>
+  )
+}
+
+
+export default App
---
+++ part2/countries/src/components/Country.jsx
@@ -0,0 +1,24 @@
+const Country = (props) => {
+    const { country } = props;
+
+    const languagesArray = Object.values(country['languages'])
+        .map(language => <li key={language}>{language}</li>)
+
+    return (
+        <div>
+            <h1>{country['name']['common']}</h1>
+            <p>{`capital ${country['capital']}`}</p>
+            <p>{`area ${country['area']}`}</p>
+
+            <h3>languages:</h3>
+            <ul>
+                {languagesArray}
+            </ul>
+
+            <img src={country['flags']['png']} alt={country['flags']['alt']} />
+        </div>
+    )
+
+}
+
+export default Country
