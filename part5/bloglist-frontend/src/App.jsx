11ef83cc3e55359270d601b2c56f6bfb68b86422
Austin He <ahe8@my.bcit.ca>
2024-10-19 13:29:17 -0700

    add up to exercise 5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -74,9 +74,11 @@ const App = () => {
             <CreateBlogForm setBlogs={setBlogs} createNotification={createNotification} blogFormRef={blogFormRef}/> 
           </Togglable>
 
-          {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} likeBlog={likeBlog} />
-          )}
+          {
+            blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map(blog => <Blog key={blog.id} blog={blog} likeBlog={likeBlog} />)
+          }
         </div>
       }
     </>
