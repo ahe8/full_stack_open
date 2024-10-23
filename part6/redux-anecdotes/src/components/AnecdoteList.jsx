8012c726c8319bd9813c7665b4a16bda42c4ba27
Austin He <ahe8@my.bcit.ca>
2024-10-23 09:37:14 -0700

    add exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,13 @@ import { useDispatch, useSelector } from 'react-redux'
 import { addVote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => {
+        if (state.filter) {
+            return state.anecdotes.filter(anecdote => anecdote.content.includes(state.filter))
+        }
+        return state.anecdotes
+    })
+
     const dispatch = useDispatch()
 
     const vote = (id) => {
