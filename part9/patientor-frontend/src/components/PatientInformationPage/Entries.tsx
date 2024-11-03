c6fa6d9a4fea0ed8409a335e34a9f13ff6a41fe7
Austin He <ahe8@my.bcit.ca>
2024-11-03 11:29:51 -0800

    add exercise 9.25

--- part9/patientor-frontend/src/components/PatientInformationPage/Entries.tsx
+++ part9/patientor-frontend/src/components/PatientInformationPage/Entries.tsx
@@ -1,23 +1,35 @@
-import { Entry } from "../../types";
+import { useState, useEffect } from "react";
+import { Diagnosis, Entry } from "../../types";
+
+import diagnosisService from '../../services/diagnoses';
 
 interface EntriesProps {
     entries: Entry[];
 }
 
 const Entries = (props: EntriesProps) => {
+    const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
+
     const { entries } = props;
+
+    useEffect(() => {
+        diagnosisService.getAll()
+            .then(data => setDiagnoses(data));
+    }, [entries]);
 
     return (
         <div>
             <h2>entries</h2>
             {
                 entries.map(entry => (
-                    <div>
+                    <div key={entry.id}>
                         <p>{entry.date} <i>{entry.description}</i></p>
 
                         {entry.diagnosisCodes &&
                             <ul>
-                                {entry.diagnosisCodes.map(code => <li>{code}</li>)}
+                                {entry.diagnosisCodes.map(code => (
+                                    <li key={`${entry.id}${code}`}>{code} {diagnoses.find(d => d.code === code)?.name}</li>)
+                                )}
                             </ul>
                         }
                     </div>
